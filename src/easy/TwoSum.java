fa767fffd39ca948007aad5d946fb4acbdd10eb5
fissehaye <fissehaye@gmail.com>
2018-08-20 20:39:33 -0500

    First solution in leetcode

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,34 @@
+package easy;
+
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+        int[] input = {2, 7, 11, 4, 15};
+        int sum = 22;
+
+        TwoSum twoSum = new TwoSum();
+
+        int[] result = twoSum.getIndexes(input, sum);
+
+        System.out.println(Arrays.toString(result));
+
+    }
+
+    public int[] getIndexes(int[] input, int sum) {
+
+        int[] result = new int[2];
+        for (int i = 0; i < input.length; i++) {
+
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] + input[j] == sum) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
